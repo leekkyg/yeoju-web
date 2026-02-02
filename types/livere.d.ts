2fa78d809c05bef3e2567bcdfd0bc4bd883790c2
이강용 <kkyg9300@gmail.com>
2026-02-02 21:58:24 +0900

    유저최종변경

--- types/livere.d.ts
+++ types/livere.d.ts
@@ -1,7 +1,13 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'livere-comment': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      'client-id'?: string;
-    };
+import 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'livere-comment': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        'client-id'?: string;
+      };
+    }
   }
 }
+
+export {};
